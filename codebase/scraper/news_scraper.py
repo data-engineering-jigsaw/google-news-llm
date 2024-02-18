3817df97c5c4b2bfd3d3ea815a0aa3d83dcfd6cd
Jeff Katzy <jeffreyerickatz@gmail.com>
2024-02-18 15:47:00 -0800

    text from link added

--- codebase/scraper/news_scraper.py
+++ codebase/scraper/news_scraper.py
@@ -12,7 +12,10 @@ def clean_link(link):
     pass
 
 def get_text_from(link):
-    pass
+    response = requests.get(link)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    source_text = ''.join([str(s) for s in soup.find_all(string=True) if s.parent.name not in ['style', 'script']])
+    return source_text
 
 def clean_text(html_text):
     pass
